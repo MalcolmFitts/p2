58eb427211ec16a1f55f5407396ac7e700e902cf
MalcolmFitts <mfitts@andrew.cmu.edu>
2018-02-24 20:25:15 -0500

    working on packet functionality in backend

--- backend.c
+++ backend.c
@@ -14,15 +14,13 @@ Node* create_node(char* path, char* name, int port, int rate) {
   /* checking for successful mem allocation */
   if(!pn) return NULL;
 
-  /* cast port to uint16_t (2 byte) */
+  /* cast port to correct type - uint16_t  */
   pn->port = (uint16_t) port;
 
   /* assign other fields directly */
   pn->content_path = path;
   pn->ip_hostname = name;
   pn->content_rate = rate;
-
-
 
   return pn;
 }
@@ -77,11 +75,82 @@ int add_node(Node_Dir* nd, Node* node) {
 }
 
 
+Pkt_t* create_packet (Node* n, uint16_t s_port, unsigned int s_num, 
+  char* filename, int flag, unsigned int ack_mask) {
+
+  Pkt_t *packet = malloc(sizeof(Pkt_t));
+  P_Hdr *hdr = malloc(sizeof(P_Hdr));
+
+  if(!packet || !hdr) return NULL;
+
+  hdr->source_port = s_port;
+  hdr->dest_port = n->port;
+  hdr->seq_num = s_num;
+
+  /* CHECK - fixing size of packets to max size for now */
+  hdr->length = MAX_PACKET_SIZE;
+
+  /* determining whether packet is data or acknowledgement */
+  if(flag == PKT_FLAG_DATA) {
+    /* assign ACK field */
+    hdr->ack = (unsigned int) PKT_DAT;
+
+    /* checking file contents */
+    FILE* fp = fopen(filename, "r");
+    if(!fp) return NULL;
+    rewind(fp);
+
+    /* CHECK - fixing file buffer size to max size */
+    char f_buf[MAX_DATA_SIZE];
+
+    /* storing file contents in f_buf */
+    fread(f_buf, 1, MAX_DATA_SIZE, fp);  /* might want to save this value */
+    strcpy(packet->buf, f_buf);
+  }
+  else if(flag == PKT_FLAG_ACK) {
+    /* assign ACK field */
+    hdr->ack = (unsigned int) (PKT_ACK & ack_mask);
+
+    /* CHECK - fixing file buffer size to max size */
+    char f_buf[MAX_DATA_SIZE] = {0};
+    strcpy(packet->buf, f_buf);
+  }
+  else {
+    return NULL;
+  }
+
+  /* TODO - calculate checksum value */
+  uint16_t c = calc_checksum(hdr);
+  hdr->checksum = c;
+
+  packet->header = *hdr;
+
+  return packet;
+}
 
 
 
+uint16_t calc_checksum(P_Hdr* hdr) {
+  /* parsing source_port, dest_port and length directly */
+  uint16_t s_p = hdr->source_port;
+  uint16_t d_p = hdr->dest_port;
+  uint16_t len = hdr->length;
 
+  /* splitting up both seq num and ack into two 16-bit nums */
+  uint16_t seq_num1 = (uint16_t) (((hdr->seq_num) >> 16) & SHORT_INT_MASK);
+  uint16_t seq_num2 = (uint16_t) ((hdr->seq_num) & SHORT_INT_MASK);
 
+  uint16_t ack1 = (uint16_t) ((hdr->ack >> 16) & SHORT_INT_MASK);
+  uint16_t ack2 = (uint16_t) ((hdr->ack) & SHORT_INT_MASK);
+
+  /* binary addition of relevant values */
+  uint16_t x = s_p + d_p + len + seq_num1 + seq_num2 + ack1 + ack2;
+
+  /* one's complement of addition */
+  uint16_t res = ~x;
+
+  return res;
+}
 
 
 
--- backend.h
+++ backend.h
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define MAX_NODES 20
+#define MAX_NODES 50
 
 /* Peer Node struct */
 typedef struct Peer_Node {
@@ -20,12 +20,42 @@ typedef struct Peer_Node {
 	char* ip_hostname;	  /* ip/hostname of peer node 		  */
 } Node;
 
+
 /* Peer Node Directory struct */
 typedef struct Node_Directory {
 	int cur_nodes;        /* current nbr of nodes             */
 	int max_nodes;        /* max possible nbr of nodes        */
 	Node *(*n_array);     /* array of pointers to nodes       */
 } Node_Dir;
+
+
+/* Packet Size Constants (bytes) */
+#define P_HDR_SIZE 16
+#define MAX_DATA_SIZE 65519
+#define MAX_PACKET_SIZE 65535 
+
+/* Packet constants and definitions */
+#define SHORT_INT_MASK 0x0000FFFF
+
+/* ACK field default values */
+#define PKT_DAT 0xAAAAAAAA
+#define PKT_ACK 0x8888FFFF
+
+/* Packet type (creation) flags */
+#define PKT_FLAG_DATA 1
+#define PKT_FLAG_ACK  2
+
+/* intial value for creating ACK packet "ack" value */
+#define ACK_MASK_INIT 0xFFFFAAAA
+
+/* value of ack in ACK packet that didnt lose any files */
+#define ACK_NO_LOSS 0x8888FFFF
+
+/*	possible future flag(s):
+#define PKT_FLAG_FRAG 3
+*/
+
+
 
 /* Packet Header struct */
 typedef struct Packet_Header {
@@ -35,11 +65,47 @@ typedef struct Packet_Header {
 	uint16_t length;			/* length		- 2 bytes */
 	uint16_t checksum;			/* checksum		- 2 bytes */
 
-	int seq_num;				/* sequence num - 4 bytes */
+	unsigned int seq_num;		/* sequence num - 4 bytes */
 
-	int ack;					/* acknowledge  - 4 bytes */
+	unsigned int ack;			/* acknowledge  - 4 bytes */
 } P_Hdr;
 
+/* Packet struct */
+typedef struct Packet {
+	P_Hdr header;				/* Packet Header */
+	char buf[MAX_DATA_SIZE];	/* Packet Data   */
+} Pkt_t;
+
+
+/*
+ *  TODO 3.) write parse_packet
+ *
+ * Pkt_t* parse_packet (char* buf)
+ *		- creates packet struct by parsing input buffer
+ *
+ *	(things i need)
+ *		- parse header data
+ *			-- source port --> buf { 0,  1}
+ *			-- dest port   --> buf { 2,  3}
+ *			-- length      --> buf { 4,  5}
+ *			-- check sum   --> buf { 6,  7}
+ *			-- seq num     --> buf { 8, 11}
+ *			-- ack    	   --> buf {12, 15}
+ *
+ *		- data (rest)
+ *
+ *
+ *
+ *
+ *  TODO 4.) write request_content
+ *
+ * Pkt_t* request_content (Node* n)
+ *		- attempts to request and receive data packet from node
+ *
+ *	TODO 5.) deal with receiving requests
+ *
+ *
+ */
 
 
 /*
@@ -77,7 +143,7 @@ int check_node_content(Node* pn, char* filename);
  *			with 0 initial Nodes
  *
  *	~param: max
- *		- attempts to define max nbr of Nodes stored in dir
+ *		- attempts to define max number of Nodes stored in directory
  *		- if max > MAX_NODES, max will be overwritten with MAX_NODES.
  *
  *	~return values:
@@ -97,16 +163,60 @@ Node_Dir* create_node_dir(int max);
 int add_node(Node_Dir* nd, Node* node);
 
 
+/*  TODO  --  CHECK
+ *
+ * create_packet 
+ *		- creates packet struct to be sent/received by nodes
+ *  
+ *  ~param: ack_mask
+ *		- if this is an acknowledgement, this should indicate which packets
+ *			were received
+ *		- acknowledge 16 packets at a time
+ *		- normal value: 
+ *			0xFFFFAAAA (last 16 bits alternate)
+ *		- bad packet: 
+ *			bit representing which packet was bad is flipped (in last 16 bits)
+ *
+ *  ~param: flag
+ *		= 1 (PKT_FLAG_DATA) --> create a data packet
+ *		= 2 (PKT_FLAG_ACK)  --> create an acknowledge packet
+ *
+ *	~return values:
+ *		- returns packet with correct content on success
+ *		- returns NULL pointer on fail
+ *
+ */
+
+
+Pkt_t* create_packet (Node* n, uint16_t s_port, unsigned int s_num, 
+  char* filename, int flag, unsigned int ack_mask);
 
 
 
+/*
+ *  calc_checksum 
+ *		- calculates checksum value of a packet header struct
+ *		- does this independently of header's checksum value
+ *
+ *	~notes:
+ *		- header consists of 7 16-bit values
+ *		- checksum will be 8th
+ *		- vars:
+ *			from hdr: {source_port, dest_port, length}
+ *			seq_num1 = (uint16_t) ((seq_num >> 16) & 0xFFFF)
+ *			seq_num2 = (uint16_t) ((seq_num & 0xFFFF))
+ *			ack1 = (uint16_t) ((ack >> 16) & 0xFFFF)
+ *			ack2 = (uint16_t) (ack & 0xFFFF)
+ *
+ *		- calculation:
+ *			x = [(s_p) + (d_p) + (l) + (seq_num1) + (seq_num2)
+ *					+ (ack1) + (ack2)]
+ *			checksum = ~ x;
+ *
+ *
+ */
 
-
-
-
-
-
-
+uint16_t calc_checksum(P_Hdr* hdr);
 
 
 
