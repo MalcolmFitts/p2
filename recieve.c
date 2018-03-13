5f3d455f08610432e8ae4e9e7ad695ce679a0c38
Sam Adams <sjadams@andrew.cmu.edu>
2018-03-13 14:37:56 -0700

    DEBUG sending char[] 0

--- recieve.c
+++ recieve.c
@@ -23,14 +23,25 @@ void error(char *msg) {
 }
 
 int main(){
+  int optval;
   int my_sockfd;
   struct sockaddr_in my_addr;
-  uint16_t my_port = 9001;
+  uint16_t my_port = 8436;
   int my_addr_len = sizeof(my_addr);
   int flag;
   char recieved[BUFSIZE];
 
   my_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+ /* setsockopt: Handy debugging trick that lets
+  *  us rerun the server immediately after we kill it;
+  *  otherwise we have to wait about 20 secs.
+  *  Eliminates "ERROR on binding: Address already in use" error.
+  */
+
+  optval = 1;
+  setsockopt(my_sockfd, SOL_SOCKET, SO_REUSEADDR,
+            (const void *)&optval , sizeof(int));
 
   bzero((char *) &my_addr, my_addr_len);
   my_addr.sin_family = AF_INET;
@@ -42,6 +53,8 @@ int main(){
 
   struct sockaddr_in sender_addr;
   socklen_t sender_len;
+
+  printf("Waiting to recieve...\n");
 
   flag = recvfrom(my_sockfd, recieved, BUFSIZE, 0,
                   (struct sockaddr *) &sender_addr, &sender_len);
