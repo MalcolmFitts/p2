778714b4268eee18f7c9e961154bddb601a79098
Sam Adams <sjadams@andrew.cmu.edu>
2018-03-13 18:47:05 -0700

    CHANGE: node_hostname and node_port

--- send.c
+++ send.c
@@ -50,8 +50,8 @@ int main(){
   /* TO SEND */
   char buf[BUFSIZE];
 
-  char *node_hostname = "172.19.138.17";
-  uint16_t node_port = 9002;
+  char *node_hostname = "172.21.67.91";
+  uint16_t node_port = 8436;
   struct sockaddr_in node_addr;
   int node_addr_len;
 
