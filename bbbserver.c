6dcea71d2a4ce0f0c551447af3da77909252a8c0
MalcolmFitts <mfitts@andrew.cmu.edu>
2018-04-02 23:16:25 -0400

    pushing for test 1.24 handle_be_response debugging + server printing change stopping now

--- bbbserver.c
+++ bbbserver.c
@@ -70,14 +70,14 @@ int main(int argc, char **argv) {
   struct sockaddr_in self_addr_fe; /* front end address */
   int connfd;                       /* connection socket */
   //struct hostent *hostf;
-  char *hostaddrf;
+  //char *hostaddrf;
 
   /* back-end (node) vars */
   int sockfd_be;                    /* listening socket */
   short port_be;                    /* back-end port to use */
   struct sockaddr_in self_addr_be;  /* back-end address */
   //struct hostent *hostb;
-  char *hostaddrb;
+  //char *hostaddrb;
 
   /* client vars */
   struct sockaddr_in clientaddr;               /* client's addr */
@@ -104,17 +104,15 @@ int main(int argc, char **argv) {
   //   sizeof(self_addr_fe.sin_addr.s_addr), AF_INET);
   // hostb = gethostbyaddr((const char *)&self_addr_be.sin_addr.s_addr,
   //   sizeof(self_addr_be.sin_addr.s_addr), AF_INET);
-
-  hostaddrf = inet_ntoa(self_addr_fe.sin_addr);
-  hostaddrb = inet_ntoa(self_addr_be.sin_addr);
-
-  /* Start-up info */
-  printf("<BBBServer start-up info>\n");
-  //printf("Initialized front-end hostname: %s\n", hostf->h_name);
-  printf("Initialized front-end address: %s:%d\n", hostaddrf, port_fe);
-  //printf("Initialized back-end hostname: %s\n", hostb->h_name);
-  printf("Initialized back-end address: %s:%d\n", hostaddrb, port_be);
-  printf("\n");
+  // hostaddrf = inet_ntoa(self_addr_fe.sin_addr);
+  // hostaddrb = inet_ntoa(self_addr_be.sin_addr);
+  // /* Start-up info */
+  // printf("<BBBServer start-up info>\n");
+  // //printf("Initialized front-end hostname: %s\n", hostf->h_name);
+  // printf("Initialized front-end address: %s:%d\n", hostaddrf, port_fe);
+  // //printf("Initialized back-end hostname: %s\n", hostb->h_name);
+  // printf("Initialized back-end address: %s:%d\n", hostaddrb, port_be);
+  // printf("\n");
 
   int* be_sockfd_ptr = &sockfd_be;
 
